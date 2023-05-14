54a5c58c97da9160c6ac30a1186efa67f844ca5f
Mihai Dima <dimamihaimail@gmail.com>
2023-05-14 13:06:25 -0700

    Some db setup

--- local_db/init.sql
+++ local_db/init.sql
@@ -23,4 +23,71 @@ create table tenant_configuration (
     constraint fk_tenant_configuration_configuration foreign key (configuration_id) references configuration(id)
 );
 
+create table category(
+    id SERIAL primary key,
+    name varchar(50),
+    tenant_id int not null,
+    constraint fk_user_content_tenant foreign key (tenant_id) references tenant(id),
+    constraint uk_user_content_name_tenant unique (tenant_id, name)
+);
+
+-- Template
+create table post_template(
+    id SERIAL primary key,
+    name varchar(50) not null,
+    tenant_id int not null,
+    constraint fk_user_content_tenant foreign key (tenant_id) references tenant(id)
+);
+
+create table post_template_slot(
+    id SERIAL primary key,
+    name varchar(50),
+    parent_id int,
+    tenant_id int not null,
+    constraint fk_post_template_slot_parent foreign key (parent_id) references post_template_slot(id),
+    constraint fk_post_template_slot_tenant foreign key (tenant_id) references tenant(id)
+);
+
+create table post_template_slot_property_type(
+    id SERIAL primary key,
+    name varchar(20) not null,
+    tenant_id int not null,
+    constraint fk_post_template_slot_property_type_tenant foreign key (tenant_id) references tenant(id),
+    constraint fk_post_template_slot_property_type_name_tenant unique (tenant_id, name)
+);
+
+create table post_template_slot_property(
+    id SERIAL primary key,
+    name varchar(50) not null,
+    value varchar(256) not null,
+    property_type_id int not null,
+    post_template_slot_id int not null,
+    tenant_id int not null,
+    constraint fk_post_template_slot_property_post_template_slot foreign key (post_template_slot_id) references post_template_slot(id),
+    constraint fk_post_template_slot_property_post_template_slot_property_type foreign key (property_type_id) references post_template_slot_property_type (id),
+    constraint fk_post_template_slot_property_tenant foreign key (tenant_id) references tenant(id)
+);
+
+-- Post
+create table post(
+    id SERIAL primary key,
+    user_id int not null,
+    user_email varchar(255),
+    category_id int,
+    post_template_id int not null,
+    tenant_id int not null,
+    constraint fk_user_content_tenant foreign key (tenant_id) references tenant(id),
+    constraint fk_user_content_category foreign key (category_id) references category(id),
+    constraint fk_user_content_template foreign key (post_template_id) references post_template(id)
+);
+
+create table post_content(
+     id SERIAL primary key,
+     content text,
+     post_template_slot_id int not null,
+     tenant_id int,
+     constraint fk_user_content_tenant foreign key (tenant_id) references tenant(id),
+     constraint fk_user_content_slot foreign key (post_template_slot_id) references post_template_slot(id)
+);
+
 insert into tenant (tenant_ref, channel, language) values ('blogs-ro_RO', 'blogs', 'ro_RO');
