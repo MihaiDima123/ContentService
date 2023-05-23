759832d007e08837ce0958b34d91ea4fab280bd7
Mihai Dima <dimamihaimail@gmail.com>
2023-05-23 11:57:30 -0700

    create post controller code

--- pkg/application/entity/post_entities/Post.go
+++ pkg/application/entity/post_entities/Post.go
@@ -1,10 +1,10 @@
 package post_entities
 
 type Post struct {
-	ID             int64
-	UserId         int
-	UserEmail      string
-	CategoryId     int
-	PostTemplateId int
-	TenantId       int
+	ID             int64  `json:"id" gorm:"column:id"`
+	UserId         int    `json:"userId"  gorm:"column:user_id"`
+	UserEmail      string `json:"userEmail"  gorm:"column:user_email"`
+	CategoryId     int    `json:"categoryId"  gorm:"column:category_id"`
+	PostTemplateId int    `json:"postTemplateId" gorm:"column:post_template_id"`
+	TenantId       int    `json:"tenantId" gorm:"column:tenant_id"`
 }
--- pkg/application/restful/content/ContentController.go
+++ pkg/application/restful/content/ContentController.go
@@ -2,6 +2,7 @@ package content
 
 import (
 	"contentservice/pkg/application/customErrors"
+	"contentservice/pkg/application/entity/post_entities"
 	"contentservice/pkg/application/modules/content/interfaces"
 	"contentservice/pkg/server/log"
 	"fmt"
@@ -24,7 +25,8 @@ func (rcc *RestfulContentController) GetPostById(context *gin.Context) {
 	idParam, err := strconv.ParseInt(context.Param("id"), 10, 32)
 
 	if err != nil {
-		context.Status(http.StatusNotFound)
+		log.Warn("Bad request, could not parse id param")
+		context.Status(http.StatusBadRequest)
 		return
 	}
 
@@ -46,5 +48,21 @@ func (rcc *RestfulContentController) GetPostById(context *gin.Context) {
 }
 
 func (rcc *RestfulContentController) CreatePost(context *gin.Context) {
+	post := post_entities.Post{}
+	err := context.BindJSON(&post)
+	if err != nil {
+		log.Warn("error when parsing request body" + err.Error())
+		context.Status(http.StatusBadRequest)
+		return
+	}
 
+	id, err := rcc.contentService.Create(post)
+
+	if httpErr, ok := err.(customErrors.HTTPError); ok {
+		log.Warn("Could not create post: " + httpErr.Error())
+		context.Status(httpErr.GetStatus())
+		return
+	}
+
+	context.JSON(http.StatusOK, id)
 }
