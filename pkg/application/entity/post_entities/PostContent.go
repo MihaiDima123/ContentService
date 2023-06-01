215c19d32ee75ae6654fe1cbb2faba23a4581af0
Mihai Dima <dimamihaimail@gmail.com>
2023-06-01 12:46:48 -0700

    Added post_id to post content entity

--- pkg/application/entity/post_entities/PostContent.go
+++ pkg/application/entity/post_entities/PostContent.go
@@ -3,6 +3,7 @@ package post_entities
 type PostContent struct {
 	ID                 int    `json:"id" gorm:"column:id"`
 	Content            string `json:"content" gorm:"column:content"`
+	PostId             int    `json:"postId" gorm:"column:post_id"`
 	PostTemplateSlotId int    `json:"post_template_slot_id" gorm:"column:post_template_slot_id"`
 	TenantId           int    `json:"tenant_id" gorm:"column:tenant_id"`
 }
