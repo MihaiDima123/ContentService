da63b06adabd0e8dd4c27a613d63f849d34303ed
Mihai Dima <dimamihaimail@gmail.com>
2023-06-02 01:16:03 -0700

    Removed TODO

--- pkg/interfaces/validator/Validator.go
+++ pkg/interfaces/validator/Validator.go
@@ -2,7 +2,6 @@ package validator
 
 import "contentservice/pkg/interfaces/customerrors"
 
-// Validator TODO: Make a chain-like validator
 type Validator[T any] interface {
 	Validate(T) []customerrors.ValidationError
 }
